06592ca76836fb725c7edb937c4c87776bcc98a5
LfourZ <lfourunderscore@gmail.com>
2017-04-05 16:51:32 -0700

    Initiated (and almost finished) weaponcore

---
+++ weaponcore/lua/entities/gmod_wire_expression2/core/custom/cl_l-weaponcore.lua
@@ -0,0 +1,37 @@
+--E2Helper.Descriptions[""] = ""
+E2Helper.Descriptions["give"] = "Gives the player a weapon."
+E2Helper.Descriptions["getPrimaryAmmoType"] = "Gets the primary ammo type of the given weapon."
+E2Helper.Descriptions["giveAmmo"] = "Gives ammo to a player. (n:amount, n:type)"
+E2Helper.Descriptions["givePrimaryAmmo"] = "Gives ammo for the player's current active weapon."
+E2Helper.Descriptions["getHoldType"] = "Returns the hold type of the weapon."
+E2Helper.Descriptions["getMaxClip1"] = "Returns maximum primary clip size."
+E2Helper.Descriptions["getMaxClip2"] = "Returns maximum secondary clip size."
+E2Helper.Descriptions["getNextPrimaryFire"] = "Gets the next time the weapon can primary fire."
+E2Helper.Descriptions["getNextSecondaryFire"] = "Gets the next time the weapon can secondary fire."
+E2Helper.Descriptions["getSlot"] = "Returns the slot of the weapon (slot numbers start from 0)"
+E2Helper.Descriptions["getSlotPos"] = "Returns slot position of the weapon."
+E2Helper.Descriptions["getWeaponViewModel"] = "Returns the view model of the weapon."
+E2Helper.Descriptions["getWeaponWorldModel"] = "Returns the world model of the weapon."
+E2Helper.Descriptions["getWeight"] = "Returns the ''weight'' of the weapon, which is used when deciding which Weapon is better by the engine."
+E2Helper.Descriptions["hasAmmo"] = "Returns whether the weapon has ammo left or not. It will return 0 when there's no ammo left in the magazine and when there's no reserve ammo left."
+E2Helper.Descriptions["isScripted"] = "Checks if the weapon is a SWEP or a built-in weapon."
+E2Helper.Descriptions["isWeaponVisible"] = "Returns whether the weapon is visible."
+E2Helper.Descriptions["lastShootTime"] = "Returns the time since this weapon last fired a bullet."
+E2Helper.Descriptions["setClip1"] = "Lets you change the number of bullets in the given weapons primary clip."
+E2Helper.Descriptions["setClip2"] = "Lets you change the number of bullets in the given weapons secondary clip."
+E2Helper.Descriptions["setHoldType"] = "Sets the hold type of the weapon. This only works on scripted weapons."
+E2Helper.Descriptions["setNextPrimaryFire"] = "Sets when the weapon can fire again. Time should be based on curtime()."
+E2Helper.Descriptions["setNextSecondaryFire"] = "Sets when the weapon can alt-fire again. Time should be based on curtime()."
+E2Helper.Descriptions["runOnWeaponSwitch"] = "If set to 1, chip will run on every weapon switch."
+E2Helper.Descriptions["lastWeaponSwitchPly"] = "Returns the last player who switched weapon."
+E2Helper.Descriptions["oldWeapon"] = "Returns the last weapon switched from."
+E2Helper.Descriptions["newWeapon"] = "Returns the last weapon switched to."
+E2Helper.Descriptions["weaponSwitchClk"] = "Returns 1 if the chip is being executed because of a weapon switch. Returns 0 otherwise."
+
+
+
+
+
+
+
+
---
+++ weaponcore/lua/entities/gmod_wire_expression2/core/custom/l-weaponcore.lua
@@ -0,0 +1,185 @@
+E2Lib.RegisterExtension("weaponcore", false)
+
+__e2setcost(1)
+
+--Hooks
+
+local WeaponSwitchAlert = {}
+local runByWeaponSwitch = 0
+local lastWeaponSwitchPly = nil
+local oldWeapon = nil
+local newWeapon = nil
+
+hook.Add("PlayerSwitchWeapon","Exp2WeaponSwitch", function(ply, oldWeaponh, newWeaponh)
+    lastWeaponSwitchPly = ply
+    oldWeapon = oldWeaponh
+    newWeapon = newWeaponh
+    runByWeaponSwitch = 1
+    for e,_ in pairs(WeaponSwitchAlert) do
+        if IsValid(e) then
+            e:Execute()
+        else
+            WeaponSwitchAlert[e] = nil
+        end
+    end
+    runByWeaponSwitch = 0
+end)
+
+e2function void runOnWeaponSwitch(number activate)
+    if activate ~= 0 then
+        WeaponSwitchAlert[self.entity] = true
+    else
+        WeaponSwitchAlert[self.entity] = nil
+    end
+end
+
+e2function entity lastWeaponSwitchPly()
+    return lastWeaponSwitchPly
+end
+
+e2function entity oldWeapon()
+    return oldWeapon
+end
+
+e2function entity newWeapon()
+    return newWeapon
+end
+
+e2function number weaponSwitchClk()
+    return runByWeaponSwitch
+end
+
+--Player functions
+
+e2function void entity:give(string weapon)
+    if not this or not this:IsPlayer() then return end
+    this:Give(weapon)
+end
+
+e2function void entity:giveAmmo(number amount, number type)
+    if not this or not this:IsPlayer() then return end
+    this:GiveAmmo(amount, type, false)
+end
+
+--Weapon functions-
+
+--allowsAutoSwitchFrom
+--allowsAutoSwitchTo
+--callOnClient
+--clip1 --Exists
+--clip2 --Exists
+--defaultReload
+--getActivity
+
+e2function string entity:getHoldType()
+    if not this or not this:IsWeapon() then return end
+    return this:GetHoldType()
+end
+
+e2function number entity:getMaxClip1()
+    if not this or not this:IsWeapon() then return end
+    return this:GetMaxClip1()
+end
+
+e2function number entity:getMaxClip2()
+    if not this or not this:IsWeapon() then return end
+    return this:GetMaxClip2()
+end
+
+e2function number entity:getNextPrimaryFire()
+    if not this or not this:IsWeapon() then return end
+    return this:GetNextPrimaryFire()
+end
+
+e2function number entity:getNextSecondaryFire()
+    if not this or not this:IsWeapon() then return end
+    return this:GetNextSecondaryFire()
+end
+
+e2function number entity:getPrimaryAmmoType()
+    if not this or not this:IsWeapon() then return end
+    return this:GetPrimaryAmmoType()
+end
+
+e2function number entity:getSlot()
+    if not this or not this:IsWeapon() then return end
+    return this:GetSlot()
+end
+
+e2function number entity:getSlotPos()
+    if not this or not this:IsWeapon() then return end
+    return this:GetSlotPos()
+end
+
+e2function string entity:getWeaponViewModel()
+    if not this or not this:IsWeapon() then return end
+    return this:GetWeaponViewModel()
+end
+
+e2function string entity:getWeaponWorldModel()
+    if not this or not this:IsWeapon() then return end
+    return this:GetWeaponWorldModel()
+end
+
+e2function number entity:getWeight()
+    if not this or not this:IsWeapon() then return end
+    return this:GetWeight()
+end
+
+e2function number entity:hasAmmo()
+    if not this or not this:IsWeapon() then return end
+    if this:HasAmmo() then return 1 else return 0 end
+end
+
+e2function number entity:isScripted()
+    if not this or not this:IsWeapon() then return end
+    if this:IsScripted() then return 1 else return 0 end
+end
+
+e2function number entity:isWeaponVisible()
+    if not this or not this:IsWeapon() then return end
+    if this:IsWeaponVisible() then return 1 else return 0 end
+end
+
+e2function number entity:lastShootTime()
+    if not this or not this:IsWeapon() then return end
+    return this:LastShootTime()
+end
+
+--sendWeaponAnim
+
+e2function void entity:setClip1(number ammo)
+    if not this or not this:IsWeapon() then return end
+    this:SetClip1(ammo)
+end
+
+e2function void entity:setClip2(number ammo)
+    if not this or not this:IsWeapon() then return end
+    this:SetClip2(ammo)
+end
+
+e2function void entity:setHoldType(string name)
+    if not this or not this:IsWeapon() then return end
+    if not this:IsScripted() then return end
+    this:SetHoldType(name)
+end
+
+--setLastShootTime --Doesn't exist in this version
+
+e2function void entity:setNextPrimaryFire(number time)
+    if not this or not this:IsWeapon() then return end
+    this:SetNextPrimaryFire(time)
+end
+
+e2function void entity:setNextSecondaryFire(number time)
+    if not this or not this:IsWeapon() then return end
+    this:SetNextSecondaryFire(time)
+end
+
+--Self made functions
+
+e2function void entity:givePrimaryAmmo(number amount)
+    if not this or not this:IsPlayer() then return end
+    if not this:GetActiveWeapon() then return end
+    this:GiveAmmo(amount, this:GetActiveWeapon():GetPrimaryAmmoType())
+end
